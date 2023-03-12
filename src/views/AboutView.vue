309aa136f96ef1dc769e1db1400719530738e3f0
Reiza <taimichael2@gmail.com>
2023-03-12 23:47:57 +0800

    Add display field for final transcript

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,18 +2,19 @@
   <div class="about">
     <h1>This is an about page</h1>
     <button @click="handleMicPress">
-      {{ micIsActive ? 'Stop microphone' : 'Start microphone' }}
+      {{ isRecording ? 'Stop microphone' : 'Start microphone' }}
     </button>
     <div>{{ micStateText }}</div>
     <div>{{ clientStateText }}</div>
     <!-- <p>{{ segment }}</p> -->
     <!-- <div>sdf</div> -->
     <div>{{ debugOutText }}</div>
-    <div>{{ transcriptText }}</div>
     <p>{{ timestamp }}</p>
-    <div>{{ intentText }}</div>
     <div>{{ tentativeText }}</div>
-    <div>{{ entitiesListText }}</div>
+    <div>{{ transcriptText }}</div>
+    <!-- <div>{{ intentText }}</div>
+    <div>{{ entitiesListText }}</div> -->
+    <button @click="handleClearPress" :disabled="!transcriptText && !transcriptText">Clear</button>
   </div>
 </template>
 
@@ -25,20 +26,22 @@ import formatDuration from 'format-duration'
 // let isVadEnabled = false;
 
 const microphone = new BrowserMicrophone()
-let micIsActive = ref(false)
+let isRecording = ref(false)
 let micStateText = ref('Placeholder')
 let clientStateText = ref('Placeholder')
 let tentativeText = ref('')
 let debugOutText = ref('')
 let transcriptText = ref('')
-let intentText = ref('')
-let entitiesListText = ref('')
+// NOTE Speechly's intent and entity detection allows the guessing of words from users when it's unclear. This feature is unnecessary, as our app trains pronunciation.
+// let intentText = ref('')
+// let entitiesListText = ref('')
 let timestamp = ref('')
 
 const client = new BrowserClient({
   appId: '20b0ff74-506d-4327-8970-73b671c98193',
   logSegments: true,
   debug: true
+  // NOTE Voice Activity Detection (VAD) reduces the CPU load of the decoder, as it prevents the decoder from processing silent parts of the audio. However, it may also introduce some errors in the resulting transcript. For the purposes of this app, VAD should probably be disabled.
   // vad: { enabled: isVadEnabled },
 })
 
@@ -54,11 +57,11 @@ const initMic = async () => {
 const handleMicPress = async () => {
   if (client.isActive()) {
     await client.stop()
-    micIsActive.value = false
+    isRecording.value = false
   } else {
     await initMic()
     await client.start()
-    micIsActive.value = true
+    isRecording.value = true
   }
 }
 
@@ -66,29 +69,22 @@ const renderTranscript = (segment) => {
   return segment.words.map((w) => w.value).join(' ')
 }
 
-const renderSegmentDetails = (intent, entities) => {
-  if (!intent.intent) return (intentText.value = '')
-  const entitiesList = entities.map((e) => `${e.value} (${e.type})`).join(', ')
-  intentText.value = intent.intent
-  entitiesList ? (entitiesListText.value = ` · entities: ${entitiesList}`) : ''
-}
+// const renderSegmentDetails = (intent, entities) => {
+//   if (!intent.intent) return (intentText.value = '')
+//   const entitiesList = entities.map((e) => `${e.value} (${e.type})`).join(', ')
+//   intentText.value = intent.intent
+//   entitiesList ? (entitiesListText.value = ` · entities: ${entitiesList}`) : ''
+// }
 
 const renderSegment = (segment) => {
   timestamp.value = formatDuration(segment.words[segment.words.length - 1].endTimestamp)
-  renderTranscript(segment)
-  renderSegmentDetails(segment.intent, segment.entities)
-  // return `<div class="segment">
-  //   <div>${timestamp}</div>
-  //   <div class="segment-content">
-  //     <div>${renderTranscript(segment)}</div>
-  //     ${renderSegmentDetails(segment.intent, segment.entities)}
-  //   </div>
-  // </div>`
+  transcriptText.value = renderTranscript(segment)
+  // renderSegmentDetails(segment.intent, segment.entities)
 }
 
-// const renderOutput = (segment) => {
-//   return `<code>${JSON.stringify(segment, undefined, 2)}</code>`
-// }
+const handleClearPress = () => {
+  transcriptText.value = ''
+}
 
 microphone.onStateChange((state) => {
   micStateText.value = state
@@ -104,7 +100,9 @@ client.onSegmentChange((segment) => {
   if (segment.isFinal) {
     // debugOutText += renderOutput(segment)
     renderSegment(segment)
-    // tentativeText = ''
+    // NOTE remove below line and transcriptText above if I only want tentativeText to show
+    tentativeText.value = ''
+    console.log(client)
   }
 })
 </script>
