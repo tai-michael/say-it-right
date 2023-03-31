77de939957b7833003139fbb66b5407b77d1c644
Reiza <taimichael2@gmail.com>
2023-04-01 02:52:19 +0800

    Implement audio playback for words using Eleven Labs API

--- src/components/WordTest.vue
+++ src/components/WordTest.vue
@@ -2,11 +2,17 @@
   <main>
     <div>Word Test</div>
     <div>{{ store.mispronouncedTestedWords[0] }}</div>
+    <button @click="play">Speaker icon</button>
     <button @click="store.wordTestCompleted = true">Next</button>
   </main>
 </template>
 
 <script setup>
+import useConvertTextToSpeech from '@/composables/useConvertTextToSpeech.js'
 import { useSuggestedListStore } from '@/stores/suggested'
 const store = useSuggestedListStore()
+
+const play = () => {
+  useConvertTextToSpeech('Espresso.', 'male')
+}
 </script>
---
+++ src/composables/useConvertTextToSpeech.js
@@ -0,0 +1,64 @@
+export default async function useConvertTextToSpeech(text, gender = 'female') {
+  try {
+    // This particular female voice ('Bella') sounds more natural than the others in both intonation and pauses when speaking longer sentences
+    let voiceId = ''
+    if (gender === 'male') voiceId = 'pNInz6obpgDQGcFmaJgB'
+    else if (gender === 'female') voiceId = 'EXAVITQu4vr4xnSDxMaL'
+
+    const apiKey = import.meta.env.VITE_ELEVENLABS_API_KEY
+    const url = `https://api.elevenlabs.io/v1/text-to-speech/${voiceId}`
+    // These are the recommended settings by Eleven Labs
+    const voiceSettings = {
+      // Increasing stability will make the voice more consistent between regenerations, but it can also make it sounds a bit monotone. On longer text fragments they recommend lowering this value.
+      stability: 0.75,
+      // High enhancement boosts overall voice clarity and target speaker similarity. Very high values can cause artifacts, so adjusting this setting to find the optimal value is encouraged.
+      similarity_boost: 0.75
+    }
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        accept: '*/*',
+        'Content-Type': 'application/json',
+        'xi-api-key': apiKey
+      },
+      body: JSON.stringify({
+        text: text,
+        voice_settings: voiceSettings
+      })
+    })
+
+    if (!response.ok) throw new Error(`Failed to generate audio for '${text}'.`)
+
+    const stream = response.body
+    const reader = stream.getReader()
+    const audioChunks = []
+
+    // eslint-disable-next-line
+    while (true) {
+      const { done, value } = await reader.read()
+      if (done) {
+        break
+      }
+      audioChunks.push(value)
+    }
+
+    // Combine the audio chunks into a single Blob
+    const audioBlob = new Blob(audioChunks)
+
+    // Create an AudioContext and decode the audio data
+    const audioContext = new AudioContext()
+    const audioBuffer = await audioContext.decodeAudioData(await audioBlob.arrayBuffer())
+
+    // Play the audio
+    const audioSource = audioContext.createBufferSource()
+    audioSource.buffer = audioBuffer
+    audioSource.connect(audioContext.destination)
+    audioSource.start(0)
+
+    await reader.closed
+    console.log('Audio stream has ended')
+  } catch (error) {
+    console.log(error)
+  }
+}
