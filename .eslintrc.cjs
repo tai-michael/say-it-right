614cd3c30748a4e68b9707ce63205028697deb86
Reiza <taimichael2@gmail.com>
2023-04-18 03:40:24 +0800

    refactor: add typescript to project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
   root: true,
   extends: [
     'plugin:vue/vue3-essential',
-    'eslint:recommended',
+    '@vue/eslint-config-typescript/recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
