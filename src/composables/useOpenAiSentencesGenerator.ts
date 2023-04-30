a572b47c5c08a0219462191174db0b8dc7a5a989
Reiza <taimichael2@gmail.com>
2023-05-01 00:21:40 +0800

    feat: add function to generate sentences with openAI

--- src/composables/useOpenAiSentencesGenerator.ts
+++ src/composables/useOpenAiSentencesGenerator.ts
@@ -1,20 +1,20 @@
 import axios from 'axios'
 
-// NOTE Can make it so that only authenticated users can create paragraphs,
+// NOTE Can make it so that only authenticated users can create sentences,
 // in which case use 'callable functions'
 // (For more details, see: https://stackoverflow.com/a/56837672/19905038)
 // May need to create firebase function inside say-it-right project
 // Then initialize the app with it (along with auth / firestore)
 
-export default async function useOpenAiParagraphGenerator(words: string[]) {
+export default async function useOpenAiSentencesGenerator(words: string[]) {
   try {
-    const url = import.meta.env.VITE_PARAGRAPH_GENERATOR_ENDPOINT
+    const url = import.meta.env.VITE_SENTENCES_GENERATOR_ENDPOINT
     const query = words.join(', ')
     const params = { query }
     const response = await axios.get(url, { params })
     console.log(response)
     return response?.data?.choices[0]?.message?.content
   } catch (err) {
-    throw new Error(`Failed to create paragraph with openAI: ${err}`)
+    throw new Error(`Failed to create sentences with openAI: ${err}`)
   }
 }
