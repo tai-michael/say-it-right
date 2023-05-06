f7192fd6defbc744c753939d891a5f4575a928e0
Reiza <taimichael2@gmail.com>
2023-05-07 02:25:02 +0800

    fix: fix the RecorderButton so that it stops recording when expected to

--- src/components/RecorderButton.vue
+++ src/components/RecorderButton.vue
@@ -5,7 +5,6 @@
       class="recording-btn"
       @mousedown="startRecording"
       @touchstart="startRecording"
-      @mouseup="stopRecording"
       @touchend="stopRecording"
     >
       <!-- {{ isRecording ? 'Recording' : 'Hold to talk' }} -->
@@ -33,8 +32,10 @@ const clientConnected = computed(() => client.decoderOptions.connect === true)
 const attachMicrophone = async () => {
   if (microphone.mediaStream) return
   await microphone.initialize()
+  // console.log('microphone.initialize() finished')
   // @ts-ignore
   await client.attach(microphone.mediaStream)
+  // console.log('client.attach() finished')
 }
 
 // NOTE Resetting the value here is necessary, since I'm currently unable to 'detach' the mic/mediastream/client when I swap between views, meaning finalTranscript and temporaryTranscript in this component carries over to the new view
@@ -45,15 +46,24 @@ onDeactivated(() => {
 const emit = defineEmits(['recordingStarted', 'recordingStopped', 'temporaryTranscriptRendered'])
 
 const startRecording = async () => {
+  // console.log('mousedown/touchstart triggered')
   await attachMicrophone()
   await client.start()
+  // console.log('client.start() finished')
+  // console.log(`client's isActive status: ${client.isActive()}`)
   emit('recordingStarted')
+  // NOTE If mouseup happens outside an element (e.g. when user clicks, holds, then releases outside the button), the mouseup event is not captured. That's why I need to add a global event listener for it on the 'window' object.
+  window.addEventListener('mouseup', stopRecording)
 }
 
 const stopRecording = async () => {
+  // console.log('mouseup/touchend triggered')
   await client.stop()
+  // console.log('client is stopped (supposedly)')
+  // console.log(`client's isActive status: ${client.isActive()}`)
   emit('recordingStopped', finalTranscript.value)
   finalTranscript.value = ''
+  window.removeEventListener('mouseup', stopRecording)
 }
 
 interface Segment {
@@ -115,11 +125,11 @@ client.onSegmentChange((segment) => {
     // -webkit-user-select: none !important;
 
     &:active {
-      background-color: #fc4343;
-      transition: 0.3s;
-      width: 85px;
-      height: 85px;
-      transition: width 0.3s, height 0.3s;
+      background-color: #e96c6c;
+      // transition: 0.3s;
+      // width: 85px;
+      // height: 85px;
+      // transition: width 0.3s, height 0.3s;
     }
   }
 }
