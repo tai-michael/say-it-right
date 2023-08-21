821ad6171530b65fe8f744a6fe5d19a88d75c022
Reiza <taimichael2@gmail.com>
2023-08-21 23:50:45 +0800

    fix: disable Pull Refresher for widescreen

--- src/components/PullRefresher.vue
+++ src/components/PullRefresher.vue
@@ -1,24 +1,27 @@
 <template>
-  <ion-refresher slot="fixed" @ionRefresh="handleRefresh()" v-show="isAtTop">
-    <ion-refresher-content></ion-refresher-content>
-  </ion-refresher>
+  <div v-if="isNarrowScreen">
+    <ion-refresher slot="fixed" @ionRefresh="handleRefresh()" v-show="isAtTop">
+      <ion-refresher-content></ion-refresher-content>
+    </ion-refresher>
 
-  <div
-    ref="refreshTrigger"
-    slot="fixed"
-    :overflow-scroll="false"
-    no-bounce
-    :has-bouncing="false"
-    :scroll-y="false"
-    :force-overscroll="false"
-    class="trigger"
-  ></div>
+    <div
+      ref="refreshTrigger"
+      slot="fixed"
+      :overflow-scroll="false"
+      no-bounce
+      :has-bouncing="false"
+      :scroll-y="false"
+      :force-overscroll="false"
+      class="trigger"
+    ></div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted, onUnmounted } from 'vue'
 import { IonRefresher, IonRefresherContent } from '@ionic/vue'
 
+const isNarrowScreen = window.innerWidth < 768
 const isAtTop = ref(false)
 const refreshTrigger = ref<HTMLElement | null>(null)
 const observerTop = new IntersectionObserver((entries) => {
