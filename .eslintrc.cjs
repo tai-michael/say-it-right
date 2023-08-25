26160c10db713b81c704ed42785adddf607f2391
Reiza <taimichael2@gmail.com>
2023-08-26 04:49:15 +0800

    feat: add i18n package and linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,9 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-essential',
     '@vue/eslint-config-typescript/recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    'eslint:recommended',
+    'plugin:@intlify/vue-i18n/recommended'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
@@ -22,5 +24,11 @@ module.exports = {
     ],
     '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
     'vue/no-deprecated-slot-attribute': 'off'
+  },
+  settings: {
+    'vue-i18n': {
+      localeDir: './path/to/locales/*.{json,json5,yaml,yml}', // extension is glob formatting!
+      messageSyntaxVersion: '^9.0.0'
+    }
   }
 }
