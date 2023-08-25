9fc9de67be2a75b249927ea0c8bac1da643c4a2d
Reiza <taimichael2@gmail.com>
2023-08-26 04:51:09 +0800

    feat: add rudimentary locale files

---
+++ src/translations/main.ts
@@ -0,0 +1,31 @@
+import en from './en.json'
+import zhCN from './zh-CN.json'
+import ja from './ja.json'
+
+// import countriesEn from '.shared/countries/en.json'
+// import countriesZh from '.shared/countries/zh.json'
+
+export default {
+  en: {
+    ...en
+  },
+  'zh-CN': {
+    ...zhCN
+  },
+  ja: {
+    ...ja
+  }
+}
+
+// export default {
+//   messages: {
+//     en: {
+//       ...en,
+//       ...countriesEn
+//     },
+//     zh: {
+//       ...zh,
+//       ...countriesZh
+//     }
+//   }
+// }
